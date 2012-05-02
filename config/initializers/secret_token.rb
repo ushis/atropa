dcf097564d13c54a4536f8bc1a51b6ae2acc25e1
ushi <ushi@porkbox.net>
2012-05-02 14:23:00 +0200

    init rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atropa::Application.config.secret_token = 'f481f34c0c771b3468fa724724ce595e6a9de9f39654d132b70e384db884b0e830172b6a593bc16bd2ac14ebe65364c6677aa1c43f5c7bd3a22195cb50815531'
