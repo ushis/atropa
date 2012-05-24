df1814f81a647da70f731a39d74270c1e91e725e
ushi <ushi@porkbox.net>
2012-05-24 07:25:05 +0200

    Using https everywhere
    - Added https to all urls in VideoHelper

--- app/helpers/video_helper.rb
+++ app/helpers/video_helper.rb
@@ -1,10 +1,10 @@
 module VideoHelper
 
-  URLS = {'vimeo'   => 'http://vimeo.com/{id}',
+  URLS = {'vimeo'   => 'https://vimeo.com/{id}',
           'youtube' => 'https://www.youtube.com/watch?v={id}'}
 
-  SOURCES = {'vimeo'   => 'http://player.vimeo.com/video/{id}?title=0&amp;byline=0&amp;portrait=0',
-             'youtube' => 'http://www.youtube.com/embed/{id}?rel=0'}
+  SOURCES = {'vimeo'   => 'https://player.vimeo.com/video/{id}?title=0&amp;byline=0&amp;portrait=0',
+             'youtube' => 'https://www.youtube.com/embed/{id}?rel=0'}
 
   def video_link(video, txt = nil)
     url = URLS[video.provider].gsub '{id}', video.vid
